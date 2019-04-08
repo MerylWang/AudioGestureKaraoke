d163e9fe6162c4ed5eea4a37fb9d094071b86eab
Meryl Wang <wangms@mit.edu>
2019-04-07 20:40:24 -0400

    gesture

--- app/main.js
+++ app/main.js
@@ -1,1 +1,43 @@
-// UI SETUP
+// TODO: UI SETUP
+
+
+// GESTURE RECOGNITION
+
+// SWIPE
+
+var controllerOptions = {enableGestures: true};
+
+Leap.loop(controllerOptions, function(frame) {
+
+  // Display Gesture object data
+  if (frame.gestures.length > 0) {
+    for (var i = 0; i < frame.gestures.length; i++) {
+      var gesture = frame.gestures[i];
+
+      if(gesture.type == "swipe") {
+          //Classify swipe as either horizontal or vertical
+          var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+          //Classify as right-left or up-down
+          if(isHorizontal){
+              if(gesture.direction[0] > 0){
+                  swipeDirection = "right";
+              } else {
+                  swipeDirection = "left";
+              }
+          } else { //vertical
+              if(gesture.direction[1] > 0){
+                  swipeDirection = "up";
+              } else {
+                  swipeDirection = "down";
+              }
+          }
+          // do someething with swipeDirection
+          console.log(swipeDirection);
+       }
+
+     }
+  }
+
+});
+
+// SCREEN TAP
