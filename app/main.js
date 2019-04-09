2321af4e421dd21013b530d2440b2fcd505087eb
Meryl Wang <wangms@mit.edu>
2019-04-08 23:58:16 -0400

    UI

--- app/main.js
+++ app/main.js
@@ -19,9 +19,9 @@ var player;
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'x5cBBXXFAPQ', // TODO: CHANGE THIS
+    height: '520',
+    width: '850',
+    videoId: 'ajtFz7RYdj4', // TODO: CHANGE THIS
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -60,6 +60,9 @@ Number.prototype.clamp = function(min, max) {
 };
 
 Leap.loop(controllerOptions, function(frame) {
+  // console.log("Leap");
+  console.log(frame);
+
 
   // Display Gesture object data
   if (frame.gestures.length > 0) {
@@ -67,13 +70,13 @@ Leap.loop(controllerOptions, function(frame) {
       var gesture = frame.gestures[i];
 
       // swipe
-      if(gesture.type == "swipe") {
+      if (gesture.type == "swipe") {
           //Classify swipe as either horizontal or vertical
           var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
           //Classify as right-left or up-down
           if(isHorizontal){
               if(gesture.direction[0] > 0){
-                  swipeDirection = "right";
+                  swipeDirection = "right"; // TODO
               } else {
                   swipeDirection = "left"; // no action
               }
@@ -90,9 +93,9 @@ Leap.loop(controllerOptions, function(frame) {
           }
           // TODO: do someething with swipeDirection
           console.log(swipeDirection);
-       } else if (gesture.type == "screenTap") {
+       } else if (gesture.type == "keyTap" || gesture.type == "screenTap") {
          // play / pause
-         console.log("screen tap");
+         console.log("tap: " + gesture.type);
          if (player.getPlayerState() == 1) { // if playing
            player.pauseVideo();
          } else if (player.getPlayerState() == 2) { // if paused
@@ -126,20 +129,22 @@ var userSaid = function(str, commands) {
 
 
 var processSpeech = function(transcript) {
-  if (userSaid(transcript, ['play, resume'])) {
+  transcript = transcript.toLowerCase();
+  // console.log("transcript: " + transcript);
+  if (userSaid(transcript, ['play', 'resume'])) {
     player.playVideo();
 
-  } else if (userSaid(transcript, ['pause'])) {
+  } else if (userSaid(transcript, ['stop', 'pause'])) {
     player.pauseVideo();
 
   } else if (userSaid(transcript, ['replay'])) {
     player.seekTo(Number('00'), true);
 
-  } else if (userSaid(transcript, ['up, loud, louder'])) {
+  } else if (userSaid(transcript, ['up', 'loud', 'louder'])) {
     var vol = player.getVolume() + 10; // TODO: make into variables
     player.setVolume(vol.clamp(0,100));
 
-  } else if (userSaid(transcript, ['down, quiet, quieter, soft, softer'])) {
+  } else if (userSaid(transcript, ['down', 'quiet', 'quieter', 'soft', 'softer'])) {
     var vol = player.getVolume() - 10; // TODO: make into variables
     player.setVolume(vol.clamp(0,100));
   }
--- app/setupSpeech.js
+++ app/setupSpeech.js
@@ -23,6 +23,7 @@ recognition.onresult = function(event) {
       otherFeedback.setContent("SPEECH DEBUG: ready");
     else
       otherFeedback.setContent("SPEECH DEBUG: " + transcript);
+      processSpeech(transcript);
   }
 
   var processed = debouncedProcessSpeech(transcript);
